6cedfb172aa740544784b8f9e28a4f9855d454ad
Sanjay Singh Rathor <sanjay.rathor1@timesgroup.com>
2026-05-07 14:40:07 +0530

    Initial Version 1.0.0

---
+++ CICDIntegartionApp/CICDIntegartionApp/CICDIntegartionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDIntegartionAppApp.swift
+//  CICDIntegartionApp
+//
+//  Created by sanjay.rathor1 on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CICDIntegartionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICDIntegartionApp/CICDIntegartionAppTests/CICDIntegartionAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  CICDIntegartionAppTests.swift
+//  CICDIntegartionAppTests
+//
+//  Created by sanjay.rathor1 on 07/05/26.
+//
+
+import Testing
+@testable import CICDIntegartionApp
+
+struct CICDIntegartionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
